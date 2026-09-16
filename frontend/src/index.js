7e3d06314b5e7d7debc78452fee749925364a462
mikepennington91-creator <mike.pennington91@gmail.com>
2026-09-16 09:52:09 +0100

    Enable print actions for generated PDFs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
+import { initialiseBrowserPdfPrinting } from "@/utils/browserPdfPrint";
+
+// Add a browser print action beside existing controlled PDF downloads.
+initialiseBrowserPdfPrinting();
 
 // Register service worker for offline support.
 if ('serviceWorker' in navigator) {
